16e5465a8a544818a1a4fb82338a06a59e37c462
Xiaohong Xu <xxa52@sfu.ca>
2023-06-01 21:40:33 -0700

    build a tweet server to upload, list and show details of tweets

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,74 @@
 var express = require('express');
 var router = express();
+var data = [];
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 router.get('/tweet', function(req, res, next) {
-  res.render('index', { title: 'tweet' , contents: 'get a list of tweet'});
+    console.log(data);
+    res.render('index', { 
+    // id: data[0].id, 
+    // message: data[0].message, 
+    // author: data[0].author,
+    // datatime: data[0].datatime
+    tweets: data
+    });
+    
+
 });
-router.get('/tweet/post', function(req, res, next) {
-  res.render('index', { title: 'tweet' , contents: 'post a new tweet'});
+
+router.post('/tweet', function(req, res, next) {
+  data.push(req.body);
+  console.log(data);
+  res.render('index', { 
+    tweets: data
+    });
+  res.end();
 });
 
 router.get('/tweet/:id', function(req, res, next) {
-  res.render('index', { title: 'tweet' , contents: `delete a tweet id ${req.params.id}`});
+  console.log(req.params.id);
+
+  let tweet = [];
+  for (let i = 0; i < data.length; i++) {
+    console.log(data[i].id);
+
+    if (data[i].id == req.params.id) {
+      tweet[0] = data[i];
+      break;
+    }
+  }  
+  console.log(tweet);
+  if (tweet.length >0) {
+    res.render('index', {tweets: tweet});
+    res.end();
+  } else {
+    res.write('tweet not found');
+    res.end();
+  }
 });
+
+
+router.delete('/tweet/:id', function(req, res, next) {
+  let index = -1;
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].id == req.params.id) {
+      index = i;
+      break;
+    }
+  }  
+  if (index !== -1) {
+    data.splice(index, 1);
+    res.write('delete successfully!');
+    res.end();
+  } else {
+    res.write('tweet not found');
+    res.end();
+  }
+});
+
+//let index = data.findIndex((tweet) => tweet.id === req.params.id);
 
 module.exports = router;
